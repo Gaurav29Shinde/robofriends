3e0ab1ca979def9f76e98121268d089187cf43c2
Gaurav29Shinde <gaurav29shinde@gmail.com>
2020-08-11 13:33:45 +0530

    Updates

---
+++ precache-manifest.c36043b48fd7ebbc02b808d9bb6b96a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa694189640bb2a1eff44ede67fea31b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aa9684d17f49f343b1b5",
+    "url": "/robofriends/static/css/main.c7f04bfa.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9684d17f49f343b1b5",
+    "url": "/robofriends/static/js/main.e6cac4f6.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
